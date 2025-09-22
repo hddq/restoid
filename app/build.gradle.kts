7d8e01bffa46cb464fb2c67617faa3bdf50ff21a
hddq <125512521+hddq@users.noreply.github.com>
2025-09-22 04:48:44 +0000

    refactor: Lower minimum SDK to 33
    This commit changes the `minSdk` version from 35 to 33 in `app/build.gradle.kts` to support a wider range of Android devices.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.hddq.restoid"
-        minSdk = 35
+        minSdk = 33
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
