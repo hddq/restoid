5548ec852f9572fe43c846c91149608e0155431a
hddq <125512521+hddq@users.noreply.github.com>
2025-11-20 12:39:52 +0000

    Bump version to 0.3.0
    - Update versionCode to 3
    - Update versionName to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "io.github.hddq.restoid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
