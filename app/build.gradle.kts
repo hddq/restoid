985bb041aeb30e0e73a6bd896770ace43575a45f
hddq <125512521+hddq@users.noreply.github.com>
2025-11-23 15:35:31 +0000

    bump version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -141,8 +141,8 @@ android {
         applicationId = "io.github.hddq.restoid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
