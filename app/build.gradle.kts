c305dac61d6ebd171e1e30e51dce5cb8de703e67
hddq <125512521+hddq@users.noreply.github.com>
2026-05-11 22:48:23 +0200

    release: 0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ val hasReleaseSigning = listOf(
     signingKeyPassword
 ).all { !it.isNullOrBlank() }
 
-val baseAppVersionCode = 15
+val baseAppVersionCode = 16
 
 android {
     namespace = "io.github.hddq.restoid"
@@ -55,8 +55,8 @@ android {
         applicationId = "io.github.hddq.restoid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 15
-        versionName = "0.5.1"
+        versionCode = 16
+        versionName = "0.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
